451b97d3d5b499e73a1c9559e15f9badb756c935
Matthew Bauer <mjbauer95@gmail.com>
2014-04-23 13:00:30 -0500

    Update world.js

--- js/world.js
+++ js/world.js
@@ -1,2 +1,18 @@
 function World() {
+  this.bird = new Bird();
+  this.tube = new Tube();
 }
+
+World.prototype.step = function() {
+  this.bird.step();
+  this.tube.step();
+};
+
+World.prototype.draw = function() {
+  this.bird.draw();
+  this.tube.draw();
+};
+
+World.prototype.hasCollision = function() {
+  return bird.collides(this.tube);
+};
